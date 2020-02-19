ee8dc19d62a06af1a40bab02e187028288313fc2
Dmitry Zhbankov <dmitry.zhbankov@gmail.com>
2020-02-19 19:32:40 +0300

    Add functions

--- func-puzzles.js
+++ func-puzzles.js
@@ -1,14 +1,79 @@
 var funcLib = {};
 
 arrLib = (function (context) {
-    context.partial=function (g,...args1) {
+    context.partial = function (g, ...args1) {
         return function (...args2) {
-            return g(...args1,...args2);
+            return g(...args1, ...args2);
         }
+    };
+
+    context.curry = function (f) {
+        return function currify() {
+            const args = Array.prototype.slice.call(arguments);
+            return args.length >= f.length ?
+                f.apply(null, args) :
+                currify.bind(null, ...args)
+        }
+    };
+
+    context.linFold = function (array, callback, initialValue = 0) {
+        let accum = initialValue;
+        for (let i = 0; i < array.length; i++) {
+            accum = callback(accum, array[i], i, initialValue);
+        }
+        return accum;
+    };
+
+    context.map = function (array, callback) {
+        let res=[];
+        for (let item of array){
+            res.push(callback(item));
+        }
+        return res;
+    };
+
+    context.filter=function (array,callback) {
+        let res=[];
+        for (let item of array){
+            if (callback(item)){
+                res.push(item);
+            }
+        }
+        return res;
+    };
+
+    context.avgEvenNums=function (array) {
+        let evens=context.filter(array,x=>x%2===0);
+        let sum=context.linFold(evens,(accum,item,index,initialVal)=>{return  accum+item});
+        return sum/evens.length;
+    };
+
+    context.lazy=function(){
+
+    };
+
+    context.memo=function(f){
+        let mem={};
+        return function (n) {
+            if (n in mem){
+                return mem[n];
+            }
+            else {
+                return mem[n]=f(n);
+            }
+        }
+    };
+
+    context.multiply=function(...args) {
+        let res=1;
+        for (let arg of args){
+            res*=arg;
+        }
+        return res;
     };
 
     return context;
 
 })(funcLib);
 
-module.exports=funcLib;
+module.exports = funcLib;
--- test/test.js
+++ test/test.js
@@ -2,14 +2,18 @@ let assert = require("assert");
 let funcLib = require("../func-puzzles");
 
 describe("func-lib", function () {
+    let sum = function (...args) {
+        let res=0;
+        for (let arg of args){
+            res+=arg;
+        }
+        return res;
+    };
+    let sum2 = function (a,b,c) {
+        return a+b+c;
+    };
+
     describe("function partial()", function () {
-        let sum = function (...args) {
-            let res=0;
-            for (let arg of args){
-                res+=arg;
-            }
-            return res;
-        };
         it("should return 9 when value is partial(sum,2)(3,4)", function () {
             assert.strictEqual(funcLib.partial(sum,2)(3,4), 9);
         });
@@ -21,6 +25,17 @@ describe("func-lib", function () {
         });
         it("should return 9 when value is partial(sum,2,3)(4)", function () {
             assert.strictEqual(funcLib.partial(sum,2, 3, 4)(5, 6 ,7), 27);
+        });
+    });
+    describe("function partial()", function () {
+        it("should return 9 when value is partial(sum,2)(3,4)", function () {
+            assert.strictEqual(funcLib.curry(sum2)(2,3,4), 9);
+        });
+        it("should return 9 when value is curry(sum2)(2)(3)(4)", function () {
+            assert.strictEqual(funcLib.curry(sum2)(2)(3)(4), 9);
+        });
+        it("should return 9 when value is curry(sum2)(2, 3)(4)", function () {
+            assert.strictEqual(funcLib.curry(sum2)(2, 3)(4), 9);
         });
     });
 });
